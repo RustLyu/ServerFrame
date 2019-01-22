5f96a602fb8fe66c50b1a6b51fd486654247cdc5
WuYan Zhong <lvshuiku@126.com>
2019-01-22 08:12:31 +0000

    add EventLoopThreadPool

--- EventLoopThreadPool.cc
+++ EventLoopThreadPool.cc
@@ -1,1 +1,31 @@
 #include "EventLoopThreadPool.h"
+
+EventLoopThreadPool::EventLoopThreadPool()
+{
+    useIndex_ = 0;
+}
+
+EventLoopThreadPool::~EventLoopThreadPool()
+{}
+
+void EventLoopThreadPool::start()
+{
+    for(int i = 0; i < threadNum_; ++i)
+    {
+        EventLoopThread* loop = new EventLoopThread();
+        threadVec_.push_back(loop);
+        loopVec_.push_back(loop->startLoop());
+    }
+}
+
+EventLoop* EventLoopThreadPool::getNextLoop()
+{
+    if(useIndex_ >= threadNum_)
+    {
+        return nullptr;
+    }
+    EventLoop* loop = loopVec_[useIndex_];
+    useIndex_ += 1;
+    return loop;
+}
+
--- EventLoopThreadPool.h
+++ EventLoopThreadPool.h
@@ -1,11 +1,30 @@
 #ifndef EVENTLOOP_THREAD_POOL_H
 #define EVENTLOOP_THREAD_POOL_H
 
-class EventLoopThreadPool
+#include <vector>
+#include <boost/noncopyable.hpp>
+#include "EventLoopThread.h"
+
+class EventLoopThreadPool:boost::noncopyable
 {
     public:
         EventLoopThreadPool();
         ~EventLoopThreadPool();
+        bool setThreadNum(int num)
+        {
+            if(num < 0)
+            {
+                return false;
+            }
+            threadNum_ = num;
+        }
+        void start();
+        EventLoop* getNextLoop();
+    private:
+        int threadNum_;
+        std::vector<EventLoopThread*> threadVec_;
+        std::vector<EventLoop*> loopVec_;
+        int useIndex_;
 };
 
 #endif // EVENTLOOP_THREAD_POOL_H
